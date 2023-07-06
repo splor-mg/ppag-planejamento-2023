68764ae38775ca1bbb9a95686b0d0bce037943f4
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-07-05 22:03:51 -0300

    adiciona log do resultado da geracao do arquivo txt

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,11 +1,11 @@
 from frictionless import Package
 import requests
-from bs4 import BeautifulSoup
 import logging
 import shutil
-from datetime import datetime
-import re
-import pytz
+import html2text
+from rich import print as rprint
+from rich.panel import Panel
+from rich.text import Text
 
 logger = logging.getLogger(__name__)
 
@@ -17,36 +17,15 @@ def extract_resource(resource_name: str, descriptor: str = 'datapackage.yaml'):
     # ATENÇÃO: Ao abrir o arquivo no Excel use o caracter | como separador de campos
     # 22:05:14 - Arquivo : programas_planejamento.txt gerado com sucesso!!!
     # Tempo de Processamento 00:01:13
-    logger.info(f"Geração de arquivo texto para {resource_name}. Aguarde esse processamento pode ser um pouco demorado!")
+    logger.info(f"Extracting resource {resource_name}")
     res = requests.get(resource.custom['api_url']) # Resource is stripping url property
     res.raise_for_status()
+    h = html2text.HTML2Text()
+    rprint(Panel(Text(h.handle(res.text), style="green")))
     if 'gerado com sucesso!' not in res.text:
         logger.error('Erro na geração do arquivo texto')
         raise Exception
     
-    soup = BeautifulSoup(res.text, 'html.parser')
-    
-    for p in soup.find_all('p'):
-        if 'gerado com sucesso!' in p.get_text():
-            logger.info(p.get_text().strip())
-            today = datetime.now(pytz.timezone('America/Sao_Paulo'))
-            time_pattern = re.compile(r'\b(\d{2}:\d{2}:\d{2})\b')
-            time_match = time_pattern.search(p.get_text().strip())
-
-            if time_match is not None:
-                time_str = time_match.group(1)
-                parsed_time = datetime.strptime(time_str, "%H:%M:%S")
-                parsed_datetime = datetime(today.year, today.month, today.day,
-                                    parsed_time.hour, parsed_time.minute, parsed_time.second, tzinfo=pytz.timezone('America/Sao_Paulo'))
-
-                logger.info(parsed_datetime)
-            else:
-                logger.warning('Horário de finalização do processamento não encontrado')
-
-    for p in soup.find_all('p'):
-        if 'Tempo de Processamento' in p.get_text():
-            logger.info(p.get_text().strip())
-
     res = requests.get(resource.custom['download_url'], stream=True)
     res.raise_for_status()
     res.raw.decode_content = True
